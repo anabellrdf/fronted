4e3b9a5fbd426828771597928d4f9808e529ce2a
Anabell <4naruiz555@gmail.com>
2026-06-02 21:44:18 -0300

    Add files via upload

---
+++ dia1anabell/script.js
@@ -0,0 +1,9 @@
+function cambiarFondo () {
+    alert("Cambiando Fondo");
+}
+
+document.getElementById("img1").addEventListener("click", cambiarFondo) ;
+document.getElementById("img2").addEventListener("click", cambiarFondo) ;
+document.getElementById("img3").addEventListener("click", cambiarFondo) ;
+document.getElementById("img4").addEventListener("click", cambiarFondo) ;
+document.getElementById("img5").addEventListener("click", cambiarFondo) ;
